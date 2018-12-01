d0277225ef9b1e5983d1d918f4dc24893f80e8aa
Mohamed Amin <devaly.dev99@gmail.com>
2018-12-01 14:13:56 +0100

    version 1.0.0

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 {
 "name" : "Streamaly",
 "version" : "1.0.2",
-"download_url" : "http://example.com/plugin-name-2.0.zip",
+"download_url" : "https://drive.google.com/uc?export=download&id=1aOORh5htxBb3FGyOUYY5jiPC2aYBCHyh",
 "sections" : {
-"description" : "Streamaly is a streaming adapter."
+"description" : "Streamaly is a streaming adapter...."
 }
 }
